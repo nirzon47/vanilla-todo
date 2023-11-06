e5f467718c8fcbb67fbd90f2b16c17e70660ebf5
Nirzon Karmakar <nirzon47@gmail.com>
2023-11-06 23:04:12 +0530

    added localStorage support

--- script.js
+++ script.js
@@ -1,18 +1,46 @@
 // Taking notes either from localStorage or initializing it
-const notes = localStorage.getItem('notes') || []
+const notes = JSON.parse(localStorage.getItem('notes')) || []
 
 // DOM elements
 const addBtn = document.getElementById('add')
 const currText = document.getElementById('current-text')
 const savedNotes = document.getElementById('saved')
+const colorPicker = document.getElementById('color-picker')
+const container = currText.parentElement.parentElement
 
 // Event Listeners
 addBtn.addEventListener('click', () => {
 	addNote()
 })
 
+colorPicker.addEventListener('input', () => {
+	setColor(colorPicker.value)
+})
+
 // Functions
 const addNote = () => {
-	notes.push({ text: currText.value })
-	console.log(notes)
+	if (!currText.value) {
+		return
+	}
+
+	const category = document.querySelector(
+		'input[name="category"]:checked'
+	).value
+
+	notes.push({
+		text: currText.value,
+		bg: container.style.backgroundColor,
+		category: category,
+	})
+	currText.value = ''
+
+	updateLocalStorage()
+}
+
+const updateLocalStorage = () => {
+	localStorage.setItem('notes', JSON.stringify(notes))
+}
+
+const setColor = (color) => {
+	container.style.backgroundColor = color
 }
